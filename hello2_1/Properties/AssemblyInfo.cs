cc88260ebb33ed38a93ada2f6b7e60f6d31defc8
alan.pellow <alan.pellow@bt.com>
2023-06-13 14:29:52 +0100

    Add project files.

---
+++ hello2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("hello2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("hello2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("3E17E892-3741-4A8D-A0B3-97D26E0D672D")]
+[assembly: AssemblyVersion("1.0.0.0")]
